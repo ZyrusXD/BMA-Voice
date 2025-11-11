4a0828c6705875a6bfc311ed968ef31f67f34165
ZyrusXD <bankgamoza@gmail.com>
2025-11-11 16:21:04 +0700

    Remove redundant WhiteNoise wrap from wsgi.py

--- bma_project/wsgi.py
+++ bma_project/wsgi.py
@@ -12,10 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.middleware import WhiteNoiseMiddleware
 
-from whitenoise.wsgi import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bma_project.settings')
 
 application = get_wsgi_application()
-
-application = WhiteNoise(application)

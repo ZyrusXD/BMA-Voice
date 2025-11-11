11971fcb2e07294d54d435ca7d393ba7ae8b4161
ZyrusXD <bankgamoza@gmail.com>
2025-11-11 15:28:05 +0700

    Configure wsgi.py to serve static files using WhiteNoise

--- bma_project/wsgi.py
+++ bma_project/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.middleware import WhiteNoiseMiddleware
+
+from whitenoise.wsgi import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bma_project.settings')
 
 application = get_wsgi_application()
+
+application = WhiteNoise(application)
